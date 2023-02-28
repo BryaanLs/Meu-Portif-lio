79b36725adaa9344de5d51d3a0a74b64b90f8053
bryaanls <bryanluccas@hotmail.com>
2023-02-28 01:05:20 -0300

    deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,8 @@ function desprendeMenu() {
     if (window.innerWidth <= 800) {
         verificaClique(menu);
     }
-    window.addEventListener('scroll', (e) => {
+    window.addEventListener('scroll', () => {
+
         if (scrollY > 0 && window.innerWidth > 800) {
             menu.classList.add('desprender');
             menu.classList.remove('prender');
@@ -44,7 +45,6 @@ function desprendeMenu() {
 
 function verificaClique(menu) {
     const menuContent = document.querySelector('.menu-content');
-
     const menuMobile = document.querySelector('.close-menu-label');
     let click = 0;
     menuMobile.addEventListener('click', () => {
@@ -52,7 +52,7 @@ function verificaClique(menu) {
         if (click % 2 === 1) {
             menu.classList.remove('prender');
             menu.classList.add('desprender');
-            menuContent.style.display = 'block'
+            menuContent.style.display = 'flex'
         }
         if (click % 2 === 0) {
             menu.style.display = 'block'
