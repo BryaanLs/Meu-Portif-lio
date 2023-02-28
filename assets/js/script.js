cee07092579f90415fa3ea25b68216ca4f1fda70
bryaanls <bryanluccas@hotmail.com>
2023-02-28 14:02:32 -0300

    deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,19 +50,18 @@ function verificaClique(menu) {
     menuMobile.addEventListener('click', () => {
         click++;
         if (click % 2 === 1) {
-            menu.classList.add('desprender');
-            menuContent.classList.add('desprenderText');
+
             menu.classList.remove('prender');
             menuContent.classList.remove('prenderText');
-
+            menu.classList.add('desprender');
+            menuContent.classList.add('desprenderText');
         }
         if (click % 2 === 0) {
-            menu.classList.add('prender');
-            menuContent.classList.add('prenderText');
             menu.style.display = 'block';
             menu.classList.remove('desprender');
             menuContent.classList.remove('desprenderText');
-
+            menu.classList.add('prender');
+            menuContent.classList.add('prenderText');
 
         }
     })
